558e2e59e08f16efaee00f911995f52319a489ff
Mahmoud Saleh <mahmoudsaleh@MACBOOKPRO-5870.station>
2026-01-16 23:11:43 +0000

    SQLite(Meals-Data): Added meals DB by using SQLite and fetched meals data and UI in the Explore-Meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,37 @@
+import { Suspense } from 'react';
 import Link from "next/link";
+import classes from './page.module.css';
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+
+
+const Meals = async () => {
+    const meals = await getMeals();
+    return <MealsGrid meals={meals} />
+}
 
 const MealsPage = () => {
+
     return (
-        <main style={{textAlign: 'center'}}>
-            <h1>Meals Page!</h1>
-        </main>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created {' '}
+                    <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>Choose your favorite recipe and cook yourself. It is easy and fun!</p>
+                <p className={classes.cta}>
+                    <Link href='/meals/share'>
+                        Share Your Favorite Recipe
+                    </Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+                    <Meals />
+                </Suspense>
+            </main>
+        </>
     )
 }
 
