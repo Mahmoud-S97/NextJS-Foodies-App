d8fb302b8477176ebbf6dae11409d66042ef0d7f
Mahmoud Saleh <mahmoudsaleh@MACBOOKPRO-5870.station>
2026-01-16 23:14:27 +0000

    Messages(Error&NotFound): Added default error and not-found pages when un expected error occurred

---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+
+const NotFound = () => {
+    return (
+        <main className='not-found'>
+            <h1>Not found</h1>
+            <p>Unfortuantely, we could not find the requested page or source.</p>
+        </main>
+    )
+}
+
+export default NotFound;
